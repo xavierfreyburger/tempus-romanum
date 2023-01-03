b58059be2ab27000bc454d4694be4d024ce3f69d
Xavier <xavier.freyburger@gmail.com>
2023-01-03 15:04:15 +0100

    Bump to latest dependencies

--- app/src/main/java/com/dev/xavier/tempusromanum/MainActivity.java
+++ app/src/main/java/com/dev/xavier/tempusromanum/MainActivity.java
@@ -65,7 +65,6 @@ public class MainActivity extends AppCompatActivity implements OnSharedPreferenc
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
